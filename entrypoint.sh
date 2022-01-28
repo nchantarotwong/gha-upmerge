4fa82ca7eaf0e7acc385326f0051563b45d378c9
Nels Chantarotwong <43188172+nchantarotwong@users.noreply.github.com>
2022-01-27 16:28:24 -0800

    removed --no-ff

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,7 +42,7 @@ echo
 set -o xtrace
 
 # Do the merge
-git merge --no-ff --no-edit $INPUT_FROM_BRANCH --allow-unrelated-histories -X theirs --squash
+git merge --no-edit $INPUT_FROM_BRANCH --allow-unrelated-histories -X theirs --squash
 
 # Push the branch
 git push --force origin $INPUT_TO_BRANCH

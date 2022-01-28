173815e331531d6d3647781a99a2c85275311f18
Nels Chantarotwong <43188172+nchantarotwong@users.noreply.github.com>
2022-01-27 16:12:33 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,7 +42,7 @@ echo
 set -o xtrace
 
 # Do the merge
-git merge --no-ff --no-edit $INPUT_FROM_BRANCH --allow-unrelated-histories --strategy-option ours
+git merge --no-ff --no-edit $INPUT_FROM_BRANCH --allow-unrelated-histories -X theirs --squash
 
 # Push the branch
 git push --force origin $INPUT_TO_BRANCH
